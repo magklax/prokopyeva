1f108086aaad67b7d838bb88d25d983cd63e788d
magklax <nadic2006@yandex.ru>
2020-07-07 21:42:49 +0300

    some corrections

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,140 @@
-$(function(){$(".main-nav__toggle").on("click",function(){$(".main-nav__list").slideToggle(300,function(){"none"===$(this).css("display")&&$(this).removeAttr("style")})})});var stickymenu=document.getElementById("main-nav"),topbtn=document.getElementById("top-btn"),stickymenuoffset=stickymenu.offsetTop;window.addEventListener("scroll",function(t){requestAnimationFrame(function(){window.pageYOffset>stickymenuoffset?(stickymenu.classList.add("main-nav--sticky"),topbtn.classList.remove("top__btn--hide"),topbtn.classList.add("top__btn")):(stickymenu.classList.remove("main-nav--sticky"),topbtn.classList.remove("top__btn"),topbtn.classList.add("top__btn--hide"))})});var sections=$("section"),nav=$("nav"),nav_height=nav.outerHeight();$(window).on("scroll",function(){$(this).scrollTop();sections.each(function(){$(this).offset().top,$(this).outerHeight()})}),nav.find("a").on("click",function(){var t=$(this).attr("href");return $("html, body").animate({scrollTop:$(t).offset().top-nav_height},500),!1});
+'use strict';
+
+(() => {
+  const topHeader = document.querySelector('#top')
+  const mainNav = topHeader.querySelector('#main-nav');
+  const navToggle = topHeader.querySelector('.main-nav__toggle');
+  const navMenu = topHeader.querySelector('.main-nav__list');
+  const topBtn = document.querySelector('#top-btn')
+
+  const mainNavHeight = mainNav.offsetHeight;
+
+  topHeader.style.height = mainNavHeight + 'px';
+
+  const openNavMenu = () => {
+    navMenu.classList.add('active');
+    navToggle.classList.add('active');
+
+    document.addEventListener('keydown', onNavMenuEscPress);
+    document.addEventListener('click', onDocumentClick, true);
+  };
+
+  const closeNavMenu = () => {
+    navMenu.classList.remove('active');
+    navToggle.classList.remove('active');
+
+    document.removeEventListener('keydown', onNavMenuEscPress);
+    document.removeEventListener('click', onDocumentClick, true);
+  };
+
+  const toggleNavMenu = () => {
+    if (navToggle.classList.contains('active')) {
+      closeNavMenu();
+    } else {
+      openNavMenu();
+    }
+  };
+
+  const onNavMenuEscPress = (evt) => {
+    if (evt.keyCode === 27) {
+      closeNavMenu();
+    }
+  };
+
+  const onDocumentClick = (evt) => {
+    evt.stopPropagation();
+
+    closeNavMenu();
+  };
+
+  const onNavToggleClick = (evt) => {
+    evt.preventDefault();
+    toggleNavMenu();
+  };
+
+  const onNavToggleEnterPress = (evt) => {
+    if (evt.keyCode === 13) {
+      evt.preventDefault();
+
+      toggleNavMenu();
+    }
+  };
+
+  navToggle.addEventListener('click', onNavToggleClick);
+  navToggle.addEventListener('keydown', onNavToggleEnterPress);
+
+  window.addEventListener('scroll', (evt) => {
+    evt.preventDefault();
+
+    if (window.pageYOffset > mainNavHeight) {
+      topBtn.classList.remove('top__btn--hide');
+      mainNav.classList.add('sticky');
+      navToggle.classList.add('sticky');
+    } else {
+      topBtn.classList.add('top__btn--hide');
+      mainNav.classList.remove('sticky');
+      navToggle.classList.remove('sticky');
+    }
+  });
+
+  const scrollToTop = () => {
+    topHeader.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  };
+
+  topBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    scrollToTop();
+  });
+
+  topBtn.addEventListener('keydown', (evt) => {
+    if (evt.keyCode === 13) {
+      evt.preventDefault();
+
+      scrollToTop();
+    }
+  });
+
+  const aboutMeSection = document.querySelector('#about-me');
+  const aboutMeArr = document.querySelectorAll('.about-me__item');
+
+  const showElement = (arr, index) => {
+    arr[index].style.opacity = '1';
+  };
+
+  window.addEventListener('scroll', (evt) => {
+    evt.preventDefault();
+
+    if (window.pageYOffset > (aboutMeSection.offsetTop - screen.height / 1.5)) {
+
+      for (let i = aboutMeArr.length - 1; i >= 0; i--) {
+        setTimeout(() => showElement(aboutMeArr, i), 600 * i);
+      }
+    }
+  });
+
+  const slider = new Siema({
+    selector: '.skills__slider',
+    duration: 400,
+    loop: true,
+    perPage: {
+      320: 3,
+      450: 4,
+      550: 5,
+      700: 6,
+    }
+  });
+
+  const prev = document.querySelector('#prev');
+  const next = document.querySelector('#next');
+
+  prev.addEventListener('click', () => slider.prev());
+  next.addEventListener('click', () => slider.next());
+
+  setInterval(() => slider.next(), 2000)
+
+})();
+
